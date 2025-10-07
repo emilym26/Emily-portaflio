8f58144dbef6ec78b51db46198312abdd3b6bdb0
emilym26 <emilym26b@gmail.com>
2025-10-07 10:04:58 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://emilybv.github.io/portafolio',
+  site: 'https://emilym26.github.io/portafolio', // 👈 tu usuario real y nombre del repo
   base: '/portafolio/',
-  output: 'static',
-  adapter: github(),
 });
-
